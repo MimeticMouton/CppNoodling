1b0477937b9f04be960fa438b9907ed5d92fa9d0
MimeticMouton <72780580+MimeticMouton@users.noreply.github.com>
2024-09-25 03:53:54 +0000

    Add sample c and c++ files

---
+++ src/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// build with: `clang src/hello.c -o bin/hello.c.out`
+
+int main(int argc, char** argv) {
+    printf("Hello from C.\n");
+    return 0;
+}
---
+++ src/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// build with: `clang++ src/hello.cpp -o bin/hello.cpp.out`
+
+int main (int argc, char** argv)
+{
+    std::cout << "Hello from cpp" << std::endl;
+    return 0;
+}
